527a8cd1b98b45fb91f0a3636f7d604e09ca4711
toanndhe140540 <toanyeuchuyenhalong123@gmail.com>
2023-02-12 06:17:27 +0700

    Change NgxBootstrap to PrimeNg

--- UI/swp490-g2-angular/src/app/app.module.ts
+++ UI/swp490-g2-angular/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StyleClassModule } from 'primeng/styleclass';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    BrowserAnimationsModule,
+    StyleClassModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
