5a8ef7990484f8380612e2fa158e9df60dea38bf
baotq2210 <baotqhe140379@fpt.edu.vn>
2023-02-14 21:39:49 +0700

    add login page

--- UI/swp490-g2-angular/src/app/app.module.ts
+++ UI/swp490-g2-angular/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPageComponent,
+    AuthenticationComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
